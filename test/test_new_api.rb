cd648c357d5102adec3d64f7e507289567f0beef
John Mair <jrmair@gmail.com>
2009-10-06 10:53:51 -0400

    fixed tests

--- test/test_new_api.rb
+++ test/test_new_api.rb
@@ -1,19 +1,24 @@
-require 'rubygems'
-require 'devil'
+direc = File.dirname(__FILE__)
 
-Devil.load_image("texture.png") do |img|
+require 'rubygems'
+require "#{direc}/../lib/devil"
+
+
+Devil.load_image("#{direc}/texture.png") do |img|
     img.negative
-    img.save("texture_neg.png")
+    img.save("#{direc}/texture_neg.png")
 end
 
-img = Devil.load_image("texture.png")
-bink = Devil.load_image("texture.png")
+img = Devil.load_image("#{direc}/texture.png")
+bink = Devil.load_image("#{direc}/texture.png")
+
 img.blur(4)
 bink.resize(50, 50)
-img.save("texture_blur.png")
-bink.save("texture_tiny.png")
+
+img.save("#{direc}/texture_blur.png")
+bink.save("#{direc}/texture_tiny.png")
 
 
-    
+                   
 
 
