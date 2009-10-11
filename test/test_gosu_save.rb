95d5b83106fadf88c18803de887c0df747699b57
John Mair <jrmair@gmail.com>
2009-10-11 01:59:20 -0400

    added tank.png and tank-modified.png. Added a bunch of new functions to ruby_il.c and ruby_ilu.c

--- test/test_gosu_save.rb
+++ test/test_gosu_save.rb
@@ -3,23 +3,20 @@ $direc = File.dirname(__FILE__)
 $LOAD_PATH.push("#{$direc}/../lib/")
 
 require 'rubygems'
-require 'opengl'
 require 'devil/gosu'
-
 
 class W < Gosu::Window
     def initialize
         super(1024, 768, false, 20)
 
         @img = Gosu::Image.new(self, "tank.png")
+        @img.circle 100, 100, 50, :color => :purple, :filled => true
 
-        @img.save("horse.png")
+        @img.save("tank-modified.png")
     end
     
     def draw
         @img.draw 100, 50,1
-        @img.draw 500, 50,1
-        save_screenshot("plunket.png")
     end
     
 end
