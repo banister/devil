1be010881475cf16ed56d69290a36098ed4a5230
John Mair <jrmair@gmail.com>
2009-10-16 00:10:38 -0400

    added include and library paths to macports directors in extconf.rb

--- ext/devil/extconf.rb
+++ ext/devil/extconf.rb
@@ -3,10 +3,14 @@ require 'mkmf'
 if RUBY_PLATFORM =~ /mingw/
     $CFLAGS += ' -I/home/john/.rake-compiler/ruby/ruby-1.8.6-p287/include/'
     $LDFLAGS += ' -L/home/john/.rake-compiler/ruby/ruby-1.8.6-p287/lib/'
+elsif RUBY_PLATFORM =~ /darwin/
+
+    # this only works if you install devil via macports
+    $CFLAGS += ' -I/opt/local/include/'
+    $LDFLAGS += ' -L/opt/local/lib/'
 end
 
-puts "platform is #{RUBY_PLATFORM}"
-if RUBY_PLATFORM =~ /win32/ || RUBY_PLATFORM =~ /mingw/
+if RUBY_PLATFORM =~ /(win32|mingw)/
     exit unless have_library("DevIL");
 else
     exit unless have_library("IL");
