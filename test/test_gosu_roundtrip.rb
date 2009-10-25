ccee49f0a83c80ba6f7bb83454223fe083951ddc
John Mair <jrmair@gmail.com>
2009-10-25 06:31:36 -0400

    added test_gosu_roundtrip to test files

---
+++ test/test_gosu_roundtrip.rb
@@ -0,0 +1,27 @@
+$direc = File.dirname(__FILE__)
+
+$LOAD_PATH.push("#{$direc}/../lib/")
+
+require 'rubygems'
+require 'devil/gosu'
+
+class W < Gosu::Window
+    def initialize
+        super(1024, 768, false, 20)
+
+        @img = Gosu::Image.new(self, "#{$direc}/texture.jpg")
+        @img = @img.to_devil.
+            emboss.
+            rotate(45).
+            to_gosu(self)
+    end
+    
+    def draw
+        @img.draw 100, 50,1
+    end
+    
+end
+
+w = W.new
+w.show
+        
